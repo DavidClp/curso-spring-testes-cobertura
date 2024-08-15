81b335f350ad51f8804615b5068c96c374c30fb9
David Clipel <davidbalk999@gmail.com>
2024-08-15 19:17:59 -0400

    feat: projeto iniciado

---
+++ src/main/java/david/api_curso_testes_cobertura/ApiCursoTestesCoberturaApplication.java
@@ -0,0 +1,13 @@
+package david.api_curso_testes_cobertura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCursoTestesCoberturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCursoTestesCoberturaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/david/api_curso_testes_cobertura/ApiCursoTestesCoberturaApplicationTests.java
@@ -0,0 +1,13 @@
+package david.api_curso_testes_cobertura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCursoTestesCoberturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
